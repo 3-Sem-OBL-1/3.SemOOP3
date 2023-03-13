5dfec80865f590036c9206e7dc9d1eeb92fd795b
Brezzor <oliv095w@edu.zealand.dk>
2023-03-13 13:43:44 +0100

    added table and a few changes to show-function

--- js/vue.js
+++ js/vue.js
@@ -2,18 +2,23 @@ const app = Vue.createApp({
   data() {
     return {
       word: null,
-      words: [],      
+      words: [],
+      showResult: false      
     }
   },
   methods: {
     show: function() {
-      let word = this.word
-      this.words = []
-      this.words.push(word.toUpperCase())
-      this.words.push(word.toLowerCase())
-      this.words.push(word.charAt(0).toUpperCase() + word.slice(1).toLowerCase())
-      this.words.push(word.slice(0, word.length-1).toLowerCase() + word.charAt(word.length-1).toUpperCase())
-      this.words.push(word.split('').reverse().join(''))
+      this.word = this.word.trim()
+      if (this.word.length > 0) {
+        let word = this.word
+        this.words = []
+        this.words.push(word.toUpperCase())
+        this.words.push(word.toLowerCase())
+        this.words.push(word.charAt(0).toUpperCase() + word.slice(1).toLowerCase())
+        this.words.push(word.slice(0, word.length-1).toLowerCase() + word.charAt(word.length-1).toUpperCase())
+        this.words.push(word.split('').reverse().join(''))
+        this.showResult = true
+      }
     }
   },
 })
